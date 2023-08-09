3dbc235a55c09a75d8752ef54897628c272c7a9e
dihanto <dihanto2306@gmail.com>
2023-08-09 21:06:02 +0700

    💄 finish portfolio built with tailwind

---
+++ dist/js/script.js
@@ -0,0 +1,21 @@
+
+// Navbar Fixed
+window.onscroll = function() {
+const header = document.querySelector('header');
+const FixNav = header.offsetTop;
+
+if (window.pageYOffset > FixNav) {
+    header.classList.add('navbar-fixed');
+} else {
+    header.classList.remove('navbar-fixed');
+}
+}
+
+// hamburger
+const hamburger  = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu')
+
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active')
+    navMenu.classList.toggle('hidden')
+});
